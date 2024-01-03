e39cda92f37c271d1b220545d55d5c601e1484f4
c0m4r <github@wolfet.pl>
2024-01-03 05:53:47 +0100

    docker files updated

---
+++ docker/default/deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+if docker compose version 2> /dev/null ; then
+    DOCKER_COMPOSE="docker compose"
+elif docker-compose version 2> /dev/null ; then
+    DOCKER_COMPOSE="docker-compose"
+else
+    echo "Docker compose not found :("
+    exit 1
+fi
+
+if [ ! "$(${DOCKER_COMPOSE} ls --format json)" == "[]" ]; then
+    echo -e "Already deployed.\n
+    ${DOCKER_COMPOSE} build                # rebuild image
+    ${DOCKER_COMPOSE} build --no-cache     # force rebuild image
+    ${DOCKER_COMPOSE} down                 # stop and remove
+    ${DOCKER_COMPOSE} up -d               # start again"
+    echo -e "\n${DOCKER_COMPOSE} ps" && ${DOCKER_COMPOSE} ps
+    exit 0
+elif [ -e Dockerfile ] && [ -e docker-compose.yml ]; then
+    ${DOCKER_COMPOSE} up -d
+    ${DOCKER_COMPOSE} ps
+    exit 0
+else
+    echo "Dockerfile or docker-compose.yml not found here $PWD"
+    exit 1
+fi
