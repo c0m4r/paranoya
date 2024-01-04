75a294d9ed58f8294060cf69ef6b2db428f8c5b6
c0m4r <github@wolfet.pl>
2024-01-04 06:50:21 +0100

    flatpak build script

---
+++ flatpak/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if ! flatpak --version &> /dev/null; then echo "flatpak not found"; exit 1; fi
+if ! flatpak-builder --version &> /dev/null; then echo "flatpak-builder not found"; exit 1; fi
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install org.freedesktop.Sdk//23.08 org.freedesktop.Platform//23.08 putty
+flatpak-builder --user --install --force-clean loki-daemonized-flatpak org.flatpak.Loki-daemonized.json
+flatpak run org.flatpak.Loki-daemonized --intense -p ./test
