214bcd9b78303d8795d1d3ef04c5ad16051b3a5c
c0m4r <github@wolfet.pl>
2024-01-04 11:35:10 +0100

    flatpak files update

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if ! flatpak --version &> /dev/null; then echo "flatpak not found"; exit 1; fi
-if ! flatpak-builder --version &> /dev/null; then echo "flatpak-builder not found"; exit 1; fi
+if ! flatpak --version 2>/dev/null; then echo "flatpak not found"; exit 1; fi
+if ! flatpak-builder --version 2>/dev/null; then echo "flatpak-builder not found"; exit 1; fi
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install org.freedesktop.Sdk//23.08 org.freedesktop.Platform//23.08
