e1ebc0cb6cd7391f64e117cea25765ea2ef87f6e
c0m4r <github@wolfet.pl>
2024-01-06 02:48:15 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ source bin/activate
 pip3 install pyinstaller black ruff
 bin/black *.py lib/*.py
 bin/ruff *.py lib/*.py
+rm -rf build/
+rm -rf dist/
 pyinstaller -F --hidden-import lib.lokilogger --hidden-import lib.helpers --hidden-import rfc5424logging --paths=lib/site-packages --paths=lib loki.py
 pyinstaller -F --hidden-import lib.lokilogger --hidden-import lib.helpers --hidden-import rfc5424logging --paths=lib/site-packages --paths=lib loki-upgrader.py
 rm -rf Loki-daemonized-$(arch)*
