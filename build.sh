5192c05c536b2b2ef28c950bb7f4ab6289d64905
c0m4r <github@wolfet.pl>
2024-01-06 02:13:36 +0100

    removed dist from source, updated build script

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+set -e
 
 source bin/activate
-pip3 install pyinstaller
+pip3 install pyinstaller black ruff
+bin/black *.py lib/*.py
+bin/ruff *.py lib/*.py
 pyinstaller -F --hidden-import lib.lokilogger --hidden-import lib.helpers --hidden-import rfc5424logging --paths=lib/site-packages --paths=lib loki.py
 pyinstaller -F --hidden-import lib.lokilogger --hidden-import lib.helpers --hidden-import rfc5424logging --paths=lib/site-packages --paths=lib loki-upgrader.py
+rm -rf Loki-daemonized-$(arch)*
+cp -r config dist/
+cp -r README.md dist/
+cp -r LICENSE dist/
+cp -r CHANGELOG dist/
+mv dist Loki-daemonized-$(arch)
+zip -r -9 -T Loki-daemonized-$(arch).zip Loki-daemonized-$(arch)
+GZIP=-9 tar cvzf Loki-daemonized-$(arch).tar.gz Loki-daemonized-aarch64
