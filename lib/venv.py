147fcd497aaa67a90c3133f9c367ae6074e26046
c0m4r <github@wolfet.pl>
2024-01-11 04:15:08 +0100

    venv checker/setup script

---
+++ lib/venv.py
@@ -0,0 +1,46 @@
+"""
+Loki (daemonized) lib/venv.py
+https://github.com/c0m4r/Loki-daemonized
+
+Loki (daemonized): Simple IOC and YARA Scanner for Linux
+Copyright (c) 2015-2023 Florian Roth
+Copyright (c) 2023-2024 c0m4r
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import os
+import sys
+
+
+def venv_setup(loki_venv_site: str) -> None:
+    """
+    venv setup
+    """
+    if os.path.exists(loki_venv_site):
+        sys.path.insert(0, loki_venv_site)
+
+
+# venv detection
+def venv_check() -> None:
+    """
+    venv check
+    """
+    python_version = f"{sys.version_info.major}.{sys.version_info.minor}"
+    loki_base_path = os.path.dirname(os.path.realpath(__file__))
+    loki_venv_path = f"{loki_base_path}/venv"
+    loki_venv_site = f"{loki_venv_path}/lib/python{python_version}/site-packages"
+
+    if not os.environ["PATH"].startswith(loki_venv_path):
+        venv_setup(loki_venv_site)
